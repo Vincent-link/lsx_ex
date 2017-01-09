ea5c666e47937b53e53cffb7fdba3db804885b60
liuhui <liuhuijisuanji@gmail.com>
2017-01-09 13:53:30 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission , only: [:edit, :destroy, :update]
 
   def index
     @jobs = Job.all
@@ -15,18 +16,10 @@ class JobsController < ApplicationController
 
   def edit
     @job = Job.find(params[:id])
-
-    if current_user != @job.user
-      redirect_to root_path, alert: "You have no permission."
-    end
   end
 
   def destroy
     @job = Job.find(params[:id])
-
-    if current_user != @job.user
-      redirect_to root_path, alert: "You have no permission."
-    end
 
     @job.destroy
     flash[:alert] = "Job Deleted"
@@ -47,10 +40,6 @@ class JobsController < ApplicationController
   def update
     @job = Job.find(params[:id])
 
-    if current_user != @job.user
-      redirect_to root_path, alert: "You have no permission."
-    end
-
     if @job.update(job_params)
       redirect_to jobs_path, notice: "Update Success"
     else
@@ -63,4 +52,13 @@ class JobsController < ApplicationController
   def job_params
     params.require(:job).permit(:title, :description)
   end
+
+  def find_group_and_check_permission
+    @job = Job.find(params[:id])
+
+    if current_user != @job.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
+
 end
