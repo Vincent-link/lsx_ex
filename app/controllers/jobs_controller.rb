1b158095c7e37112860894b8bfa208a2ab7b02ed
liuhui <liuhuijisuanji@gmail.com>
2017-01-09 13:47:11 +0800

    check owner permission when access edit/update/destroy

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,5 @@
 class JobsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @jobs = Job.all
@@ -15,14 +15,21 @@ class JobsController < ApplicationController
 
   def edit
     @job = Job.find(params[:id])
+
+    if current_user != @job.user
+      redirect_to root_path, alert: "You have no permission."
+    end
   end
 
   def destroy
     @job = Job.find(params[:id])
+
+    if current_user != @job.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+
     @job.destroy
-
     flash[:alert] = "Job Deleted"
-
     redirect_to jobs_path
   end
 
@@ -39,6 +46,10 @@ class JobsController < ApplicationController
 
   def update
     @job = Job.find(params[:id])
+
+    if current_user != @job.user
+      redirect_to root_path, alert: "You have no permission."
+    end
 
     if @job.update(job_params)
       redirect_to jobs_path, notice: "Update Success"
