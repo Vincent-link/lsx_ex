7b34a9ffbc63b0f13d0da98a3a1fb2f6a1477e47
liuhui <liuhuijisuanji@gmail.com>
2017-01-13 10:35:34 +0800

    implement wage part

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -50,7 +50,7 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email)
   end
 
   def find_job_and_check_permission
