90b8cc9529a2be97c1f0aeffa24379af3126eaa1
liuhui <liuhuijisuanji@gmail.com>
2017-01-15 03:44:14 +0800

    version-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
+
 Rails.application.routes.draw do
   devise_for :users
-  root 'jobs#index'
 
   namespace :admin do
-   resources :jobs do
-     member do
-       post :publish
-       post :hide
-     end
-   end
- end
+    resources :jobs do
+      member do
+        post :publish
+        post :hide
+      end
+      resources :resumes
+    end
+  end
 
  resources :jobs do
    resources :resumes
  end
 
+  root 'jobs#index'
 end
